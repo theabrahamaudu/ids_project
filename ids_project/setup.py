265b450c1f4ade52eecc37129574e8053214555c
Abraham Audu <ab6ril@gmail.com>
2023-05-16 17:49:33 +0100

    base project structure

---
+++ ids_project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Cloud-Based AI Intrusion Detection System for IoT Networks',
+    author='the_author',
+    license='MIT',
+)
