64073a11bf611b4a170714958405ad3e3a05c934
Abraham Audu <ab6ril@gmail.com>
2023-06-10 22:46:21 +0100

    implemented data labelling and merging

---
+++ ids_project/src/data/load_n_filter.py
@@ -0,0 +1,232 @@
+"""
+Loads all pcap files in the given directory, converts them to csv
+and lables the data in each csv file.
+
+Saves labelled files in specified directory
+"""
+
+import os
+import pandas as pd
+from tqdm import tqdm
+from pcap_to_csv import pcapng_to_csv
+import data_filters
+
+def scan_directory(directory: str, extension: str) -> list:
+    """Check specified directory and return list of files with
+    specified extension
+
+    Args:
+        directory (str): path string to directory e.g. "./the/directory"
+        extension (str): extension type to be searched for e.g. ".csv"
+
+    Returns:
+        list: strings of file names with specified extension
+    """    
+    files: list = []
+    for filename in os.listdir(directory):
+        if filename.endswith(extension):
+            files.append(filename)
+    return files
+
+
+def load_and_filter_files(directory_path: str,
+                          pcap_files_list: list,
+                          destination_path: str,
+                          merge: bool=False,
+                          pick_up: bool=False):
+
+    # Scan destination path for existing csv files
+    existing_csv = scan_directory(destination_path,".csv")
+    
+    print(f"Converting {len(pcap_files_list)} pcap files to csv\n")
+
+    # Initialize fails counter
+    FAILS = 0
+
+    # Loop through the files in the directory and load each one
+    for filename in pcap_files_list:
+
+        if pick_up==True and str(filename[:-5]+".csv") in existing_csv:
+            print(f"{filename} already converted\n")
+        else:
+
+            try:
+                print(f"Coverting {filename} to csv...")
+                csv_file = pcapng_to_csv(
+                    PCAPNG_FILE=str(directory_path+"/"+filename),
+                    CSV_FOLDER_PATH='./data/interim'
+                )
+                data_df = pd.read_csv(str("./data/interim"+"/"+filename[:-5]+".csv"))
+                
+                print(f"Adding labels to {filename[:-5]}.csv...\n")
+                # filter the files using the specific filter function for each file
+                if "benign-dec.pcap" in filename:
+                    data_labelled = data_filters.benign_dec(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "mitm-arpspoofing-1-dec.pcap" in filename or\
+                "mitm-arpspoofing-2-dec.pcap" in filename or\
+                "mitm-arpspoofing-3-dec.pcap" in filename:
+                    data_labelled = data_filters.mitm_arpspoofing_1_3_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "mitm-arpspoofing-4-dec.pcap" in filename or\
+                "mitm-arpspoofing-5-dec.pcap" in filename or\
+                "mitm-arpspoofing-6-dec.pcap" in filename:
+                    data_labelled = data_filters.mitm_arpspoofing_4_6_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "dos-synflooding-1-dec.pcap" in filename or\
+                "dos-synflooding-2-dec.pcap" in filename:
+                    data_labelled = data_filters.dos_synflooding_1_2_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "dos-synflooding-3-dec.pcap" in filename:
+                    data_labelled = data_filters.dos_synflooding_3_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "dos-synflooding-4-dec.pcap" in filename or\
+                "dos-synflooding-5-dec.pcap" in filename or\
+                "dos-synflooding-6-dec.pcap" in filename:
+                    data_labelled = data_filters.dos_synflooding_4_6_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "scan-hostport-1-dec.pcap" in filename:
+                    data_labelled = data_filters.scan_hostport_1_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "scan-hostport-2-dec.pcap" in filename:
+                    data_labelled = data_filters.scan_hostport_2_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "scan-hostport-3-dec.pcap" in filename:
+                    data_labelled = data_filters.scan_hostport_3_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "scan-hostport-4-dec.pcap" in filename:
+                    data_labelled = data_filters.scan_hostport_4_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "scan-hostport-5-dec.pcap" in filename:
+                    data_labelled = data_filters.scan_hostport_5_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "scan-hostport-6-dec.pcap" in filename:
+                    data_labelled = data_filters.scan_hostport_6_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "scan-portos-1-dec.pcap" in filename or\
+                "scan-portos-2-dec.pcap" in filename or\
+                "scan-portos-3-dec.pcap" in filename:
+                    data_labelled = data_filters.scan_portos_1_3_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "scan-portos-4-dec.pcap" in filename or\
+                "scan-portos-5-dec.pcap" in filename or\
+                "scan-portos-6-dec.pcap" in filename:
+                    data_labelled = data_filters.scan_portos_4_6_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "mirai-udpflooding-1-dec.pcap" in filename or\
+                "mirai-udpflooding-2-dec.pcap" in filename or\
+                "mirai-udpflooding-3-dec.pcap" in filename or\
+                "mirai-udpflooding-4-dec.pcap" in filename:
+                    data_labelled = data_filters.mirai_udpflooding_1_4_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "mirai-ackflooding-1-dec.pcap" in filename or\
+                "mirai-ackflooding-2-dec.pcap" in filename or\
+                "mirai-ackflooding-3-dec.pcap" in filename or\
+                "mirai-ackflooding-4-dec.pcap" in filename:
+                    data_labelled = data_filters.mirai_ackflooding_1_4_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "mirai-httpflooding-1-dec.pcap" in filename or\
+                "mirai-httpflooding-2-dec.pcap" in filename or\
+                "mirai-httpflooding-3-dec.pcap" in filename or\
+                "mirai-httpflooding-4-dec.pcap" in filename:
+                    data_labelled = data_filters.mirai_httpflooding_1_4_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "mirai-hostbruteforce-1-dec.pcap" in filename or\
+                "mirai-hostbruteforce-3-dec.pcap" in filename or\
+                "mirai-hostbruteforce-5-dec.pcap" in filename:
+                    data_labelled = data_filters.mirai_hostbruteforce_1_3_n_5_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+
+                if "mirai-hostbruteforce-2-dec.pcap" in filename or\
+                "mirai-hostbruteforce-4-dec.pcap" in filename:
+                    data_labelled = data_filters.mirai_hostbruteforce_2_n_4_dec_filter(data_df)
+                    data_labelled.to_csv(str(destination_path+"/"+filename[:-5]+".csv"),
+                                        index=False, header=True, mode='w')
+            except Exception as e:
+                print(f"An error occured with file '{filename}': \n", e)
+                FAILS+=1
+                
+
+    print(f"\n{len(pcap_files_list) - FAILS} pcap files labelled and saved to csv\n",
+          f"{FAILS} files could not be converted. Read terminal logs for details")
+
+    # If merge is set to true, create a csv file with all the data
+    if merge is True:
+        print(f"Merging {len(pcap_files_list)} csv files...")
+        # Initialize counter
+        COUNT = 0
+
+        for filename in tqdm(pcap_files_list,
+                             desc="Merging Files",
+                             unit=" files",
+                             total=len(pcap_files_list)):
+
+            # Increment counter
+            COUNT+=1
+            labelled_dataframe = pd.read_csv(str(destination_path+"/"+filename[:-5]+".csv"))
+
+            if COUNT == 1:
+                labelled_dataframe.to_csv(str(destination_path+"/"+"all_data_labelled.csv"),
+                                          index=False, header=True, mode='w')
+            else:
+                labelled_dataframe.to_csv(str(destination_path+"/"+"all_data_labelled.csv"),
+                                          index=False, header=False, mode='a')
+        
+        print(f"|| {COUNT} csv files merged and saved to {destination_path} ||")
+
+        
+        
+if __name__ == '__main__':
+    # Specify the directory to scan
+    directory_path = "./data/external"
+
+    # Labelled data directory
+    destination_path = './data/labelled'
+
+    # Call the function to scan the directory and get the list of .pcap files
+    pcap_files_list = scan_directory(directory_path, ".pcap")
+    print(pcap_files_list)
+
+    # the_list = ['dos-synflooding-4-dec.pcap', 'mirai-udpflooding-3-dec.pcap', 'scan-portos-4-dec.pcap',
+    #             'mitm-arpspoofing-4-dec.pcap']
+
+    load_and_filter_files(directory_path=directory_path,
+                          pcap_files_list=pcap_files_list,
+                          destination_path=destination_path,
+                          merge=False,
+                          pick_up=True)
---
+++ ids_project/src/data/pcap_to_csv.py
@@ -0,0 +1,215 @@
+"""
+External pcapng file version
+This module is used to read and convert packet data from pcapng file to csv format
+
+"""
+
+import re
+import pyshark
+import pandas as pd
+from tqdm import tqdm
+
+
+
+def pcapng_to_csv(PCAPNG_FILE: str, CSV_FOLDER_PATH: str, CSV_NAME: str=None, BATCH_SIZE: int = 1000,
+                  data_desc_path: str = './data/external/dataset_description.xlsx',
+                  ):
+    """_summary_
+
+    Args:
+        PCAPNG_FILE (str): _description_
+        CSV_FILE_PATH (str): _description_
+        BATCH_SIZE (int, optional): _description_. Defaults to 1000.
+        data_desc_path (str, optional): _description_. Defaults to '../data/external/dataset_description.xlsx'.
+
+    Returns:
+        _type_: _description_
+    """    
+    # Load data description
+    data_description = pd.read_excel(data_desc_path, 
+                                 sheet_name='Files & description', 
+                                 header=2)
+
+    # Create an empty list to store the packets
+    packets_list = []
+    packets_df = pd.DataFrame()  # Initialize the final DataFrame
+
+    # Open the pcapng file using FileCapture
+    capture = pyshark.FileCapture(PCAPNG_FILE)
+
+    # Get pcapng filename
+    file_name = re.search(r'([^/\\]+)\.\w+$', PCAPNG_FILE).group(1)
+
+    # Set CSV file path
+    if CSV_NAME != None:
+        CSV_FILE_PATH = str(CSV_FOLDER_PATH+"/"+CSV_NAME+".csv")
+    else:
+        CSV_FILE_PATH = str(CSV_FOLDER_PATH+"/"+file_name+".csv")
+
+    # Initialize counter and limit
+    COUNT = 0
+    LIMIT = int(data_description[data_description['File Name']==\
+                             str(file_name+".pcap")]\
+                            ['# Total Packets'].iloc[0])
+
+    # Initialize batch counter
+    BATCH_COUNT = 0
+
+    # Iterate over the packets
+    for packet in tqdm(capture, desc="Reading packets", unit=" packets", total=LIMIT):
+        if COUNT >= LIMIT:
+            break
+
+        # Increment the loop counter
+        COUNT +=1
+
+        # Create empty packet dictionary
+        packet_data = {}
+
+        # Extract IP fields from the packet into the dictionary
+        ip_fields = [
+            'version', 'hdr_len', 'dsfield', 'dsfield_dscp', 'dsfield_ecn', 'len', 'id', 'flags',
+            'flags_rb', 'flags_df', 'flags_mf', 'frag_offset', 'ttl', 'proto', 'checksum',
+            'checksum_status', 'src', 'addr', 'src_host', 'host', 'dst', 'dst_host'
+            ]
+
+        if 'IP' in packet:
+            packet_data.update({
+                'timestamp': packet.sniff_time.timestamp(),
+                **{f'ip_{field}': getattr(packet.ip, field) if hasattr(packet.ip, field) else '' for field in ip_fields}
+            })
+        else:
+            packet_data.update({
+                **{f'ip_{field}': None for field in ip_fields}
+            })
+
+        # Extract TCP fields from the packet into the dictionary
+        tcp_fields = [
+            'srcport', 'dstport', 'port', 'stream', 'completeness', 'len', 'seq', 'seq_raw',
+            'nxtseq', 'ack', 'ack_raw', 'hdr_len', 'flags', 'flags_res', 'flags_ae', 'flags_cwr',
+            'flags_ece', 'flags_urg', 'flags_ack', 'flags_push', 'flags_reset', 'flags_syn',
+            'flags_fin', 'flags_str', 'window_size_value', 'window_size', 'window_size_scalefactor',
+            'checksum', 'checksum_status', 'urgent_pointer', '', 'time_relative', 'time_delta',
+            'analysis', 'analysis_bytes_in_flight', 'analysis_push_bytes_sent'
+            ]
+
+        if 'TCP' in packet:
+            packet_data.update({
+                **{f'tcp_{field}': getattr(packet.tcp, field) if hasattr(packet.tcp, field) else '' for field in tcp_fields}
+            })
+        else:
+            packet_data.update({
+                **{f'tcp_{field}': None for field in tcp_fields}
+            })
+
+        # Extract UDP fields from the packet into the dictionary
+        udp_fields = [
+            'srcport', 'dstport', 'port', 'length', 'checksum', 'checksum_status', 'stream',
+            'time_relative', 'time_delta'
+            ]
+
+        if 'UDP' in packet:
+            packet_data.update({
+                **{f'udp_{field}': getattr(packet.udp, field) if hasattr(packet.udp, field) else '' for field in udp_fields}
+            })
+        else:
+            packet_data.update({
+                **{f'udp_{field}': None for field in udp_fields}
+            })
+
+        # Extract ETH fields from the packet into the dictionary
+        eth_fields = [
+            'dst', 'dst_resolved', 'dst_oui', 'dst_oui_resolved', 'addr', 'addr_resolved', 'addr_oui',
+            'addr_oui_resolved', 'dst_lg', 'lg', 'dst_ig', 'ig', 'src', 'src_resolved', 'src_oui',
+            'src_oui_resolved', 'src_lg', 'src_ig', 'type'
+            ]
+
+        if 'ETH' in packet:
+            packet_data.update({
+                **{f'eth_{field}': getattr(packet.eth, field) if hasattr(packet.eth, field) else '' for field in eth_fields}
+            })
+        else:
+            packet_data.update({
+                **{f'eth_{field}': None for field in eth_fields}
+            })
+
+        # Extract ICMP fields from the packet into the dictionary
+        icmp_fields = [
+            'type', 'code', 'checksum', 'checksum_status', 'ident', 'ident_le', 'seq',
+            'seq_le', 'data_len'
+            ]
+
+        if 'ICMP' in packet:
+            packet_data.update({
+                **{f'icmp_{field}': getattr(packet.icmp, field) if hasattr(packet.icmp, field) else '' for field in icmp_fields}
+            })
+        else:
+            packet_data.update({
+                **{f'icmp_{field}': None for field in icmp_fields}
+            })
+
+        # Extract ARP fields from the packet into the dictionary
+        arp_fields = [
+            'hw_type', 'proto_type', 'hw_size', 'proto_size', 'opcode', 'src_hw_mac',
+            'src_proto_ipv4', 'dst_hw_mac', 'dst_proto_ipv4'
+            ]
+
+        if 'ARP' in packet:
+            packet_data.update({
+                **{f'arp_{field}': getattr(packet.arp, field) if hasattr(packet.arp, field) else '' for field in arp_fields}
+            })
+        else:
+            packet_data.update({
+                **{f'arp_{field}': None for field in arp_fields}
+            })
+
+        # Append the dictionary to the list
+        packets_list.append(packet_data)
+
+        # Convert packets_list to DataFrame after batch size is reached
+        if len(packets_list) >= BATCH_SIZE:
+            batch_df = pd.DataFrame(packets_list)
+            packets_df = pd.concat([packets_df, batch_df], ignore_index=True)
+            packets_list = []  # Clear the packets_list
+
+            # Increment the batch counter
+            BATCH_COUNT += 1
+
+            # save batch to csv
+            if BATCH_COUNT == 1:
+                # First update: Clear the existing file and add headers
+                packets_df.to_csv(CSV_FILE_PATH, index=False, header=True, mode='w')
+            else:
+                # Subsequent updates: Append rows without headers
+                packets_df.to_csv(CSV_FILE_PATH, index=False, header=False, mode='a')
+            packets_df = pd.DataFrame()  # Clear the packets_df
+
+    # Convert any remaining packets in packets_list to DataFrame
+    if len(packets_list) > 0:
+        batch_df = pd.DataFrame(packets_list)
+        packets_df = pd.concat([packets_df, batch_df], ignore_index=True)
+
+    # Save any remaining packets_df to a CSV file
+    if not packets_df.empty:
+        packets_df.to_csv(CSV_FILE_PATH, index=False, header=False, mode='a')
+
+    packets_csv: pd.DataFrame = pd.read_csv(CSV_FILE_PATH)
+    return packets_csv
+
+# ----------------------------------------------------------------
+# # Demo
+# if __name__ == '__main__':
+
+#     # Specify the path to the pcapng file
+#     PCAPNG_FILE_PATH = './data/external/dos-synflooding-5-dec.pcap'
+
+#     # Specify destination file path
+#     CSV_FILE = './data/interim'
+
+#     # Set the batch size for DataFrame conversion and CSV file path
+#     # BATCH_SIZE = 1000  # Adjust the batch size as needed
+
+#     benign = pcapng_to_csv(PCAPNG_FILE=PCAPNG_FILE_PATH,
+#                   CSV_FOLDER_PATH=CSV_FILE)
+
+#     print(benign.head())
