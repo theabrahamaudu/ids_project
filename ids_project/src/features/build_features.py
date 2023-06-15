587f305d654a430f32a20ea3352f5c5a1a3bed20
Abraham Audu <ab6ril@gmail.com>
2023-06-15 19:23:36 +0100

    Added preprocessing pipeline files

--- ids_project/src/features/build_features.py
+++ ids_project/src/features/build_features.py
@@ -2,6 +2,7 @@
 Transform, encode and select features from dataset
 """
 
+import warnings
 import pandas as pd
 from pandas import DataFrame
 import joblib
@@ -11,21 +12,64 @@ import ipaddress
 from sklearn.preprocessing import OneHotEncoder
 from tqdm import tqdm
 
+# Model and Optimization 
+from sklearn.preprocessing import StandardScaler, Normalizer, MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import RFE
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import f1_score, classification_report
 
-def load_dataset(path: str) -> DataFrame:
-    """ Load csv dataset to Pandas Dataframe
+# Ignore  "SettingWithCopyWarning" 
+from pandas.errors import SettingWithCopyWarning
+warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
+
+
+def load_data(path: str, Numpy: bool=True):
+    """ Load csv data to Numpy Array or Pandas Dataframe.
+    
+    Primarily designed for loading preprocessed data from memory.
 
     Args:
         path (str): path to csv file
 
     Returns:
-        DataFrame: DataFrame of dataset
+        NDArray or DataFrame: NDArray or DataFrame of data
     """    
-    return pd.read_csv(path)
+    if Numpy==True:
+        return np.genfromtxt(path, delimiter=',')
+    else:
+        return pd.read_csv(path)
+
+
+def get_features(data: DataFrame, optimal_features: list, train: bool=True) -> DataFrame:
+    """Filter out optimal features from feature selection experiments
+
+    Args:
+        data (DataFrame): DataFrame with all features
+        optimal_features (list, optional): List of optimal features. 
+                                           Defaults to predefined list.
+
+    Returns:
+        DataFrame: DataFrame with only optimal features
+    """ 
+    if train==True:
+        # Add 'label' to columns to be used
+        train_features = optimal_features.copy()
+        train_features.append('label')
+        # Extract columns to be sued from full dataset
+        data_optimal_features = data[train_features]
+
+        return data_optimal_features
+
+    # Extract columns to be sued from full dataset   
+    data_optimal_features = data[optimal_features]
+
+    return data_optimal_features
 
 
 def label_data(data: DataFrame, column: str, label_mapping: dict) -> DataFrame:
-    """_summary_
+    """
+    Labels specified column in dataset based on mapping provided
 
     Args:
         data (DataFrame): _description_
@@ -36,7 +80,7 @@ def label_data(data: DataFrame, column: str, label_mapping: dict) -> DataFrame:
         DataFrame: _description_
     """    
 
-    progress_bar = tqdm(total=len(data), desc='Label Encoding')
+    progress_bar = tqdm(total=len(data), desc='Label Encoding', unit=" rows")
 
     for i, value in enumerate(data[column]):
         data.at[i, column] = label_mapping[value]
@@ -47,3 +91,233 @@ def label_data(data: DataFrame, column: str, label_mapping: dict) -> DataFrame:
     data[column] = data[column].astype('int64')
 
     return data
+
+
+def undersample_data(data: DataFrame, label_column: str) -> DataFrame:
+    """Undersample data points with label count greater than the mean label count of the dataset.
+
+    Undersampling Strategy:
+        (label count / total label count) * mean label count
+
+    This ensures that the overpopulated labels are trimmed proportionally, as opposed to 
+    trimming all oversampled points to a fixed number, thus retaining the underlying
+    difference in frequency, but still preventing excessive skew in distribution.
+
+    Args:
+        data (DataFrame): DataFrame to be undersampled
+        label_column (str): Column holding the labels
+
+    Returns:
+        DataFrame: Undersampled DataFrame
+    """    
+    value_counts = data[label_column].value_counts()
+    mean_count = value_counts.mean()
+
+    undersampled_data = pd.DataFrame(columns=data.columns)
+
+    for value, count in value_counts.items():
+        if count > mean_count:
+            undersampled_count = int((count / value_counts.sum()) * mean_count)
+            subset = data[data[label_column] == value].sample(n=undersampled_count, random_state=42)
+            undersampled_data = pd.concat([undersampled_data, subset], ignore_index=True)
+        else:
+            subset = data[data[label_column] == value]
+            undersampled_data = pd.concat([undersampled_data, subset], ignore_index=True)
+
+    # Randomize the undersampled data
+    randomized_data = undersampled_data.sample(frac=1, random_state=42)
+
+    return randomized_data
+
+
+def convert_to_float(data: DataFrame) -> DataFrame:
+    """Parse Dataframe columns and convert all values to float
+
+    Args:
+        data (DataFrame): DataFrame to be parsed
+
+    Returns:
+        DataFrame: DataFrame with all columns as floats
+    """
+    if 'tcp_flags_str' in data.columns or 'tcp_flags_fin' in data.columns:
+        # Drop the 'tcp_flags_str' and 'tcp_flags_fin' column
+        data = data.drop(['tcp_flags_str', 'tcp_flags_fin'], axis=1)  
+    
+    # Setup progress bar
+    progress_bar = tqdm(total=(len(data.columns) * len(data)),
+                        desc='Converting to float64',
+                        unit = ' data points')
+    
+    for col in data.columns:
+
+        # Initialize list of converted values for each column
+        converted_values = []
+
+        for value in data[col]:
+
+            if pd.isna(value):
+                converted_values.append(-3)  # Assign -3 for NaN values
+            elif isinstance(value, (int, float)):
+                converted_values.append(float(value))  # Convert numbers to float
+            elif isinstance(value, str):
+                try:
+                    if value.startswith('0x'):
+                        converted_values.append(int(value, 16))  # Convert hexadecimal string to int
+                    elif '.' in value:
+                        parts = value.split('.')
+                        if len(parts) == 4:
+                            ip = ipaddress.ip_address(value)
+                            converted_values.append(int(ip))  # Convert IP address to int
+                        else:
+                            converted_values.append(float(value))  # Convert string representation of float to float
+                    else:
+                        converted_values.append(-4)  # Assign -4 for regular text values
+                except ValueError:
+                    converted_values.append(-4)  # Assign -4 for text that cannot be converted
+                except ipaddress.AddressValueError:
+                    converted_values.append(-4)  # Assign -4 for invalid IP addresses
+            else:
+                converted_values.append(-4)  # Assign -4 for other non-convertible values
+            
+            # update progress bar
+            progress_bar.update(1)
+
+        data[col] = converted_values
+        data[col] = data[col].astype('float64')  # Typecast the column to float64
+
+    # close progress bar
+    progress_bar.close()
+
+    return data
+
+
+def split(data: DataFrame, target_col: str):
+    """Split data into train and test sets
+
+    Args:
+        data (DataFrame): DataFrame to be split
+        target_col (str): Prediction target column
+
+    Returns:
+        DataFrame(s): Four dataframes in this order:
+            X_train, X_test, y_train, y_test
+    """    
+    X = data.drop(target_col, axis=1) # Inputs
+    y = data[target_col] # Target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, 
+                                                        random_state=42, 
+                                                        stratify=y,
+                                                        shuffle=True)
+    
+    return X_train, X_test, y_train, y_test
+
+
+def scale_data(data: DataFrame=None, 
+               X_train: DataFrame=None, 
+               X_test: DataFrame=None, 
+               train: bool=True):
+    """Scale input data and return numpy array of the data
+
+    If train is set to true, fits scaler to X_train, saves scaler to memory and
+    transforms X_train and X_test to scaled numpy arrays.
+
+    If train is set to false, loads scaler from memory, transforms data and
+    returns scaled data numpy array
+
+    Args:
+        data (DataFrame, optional): _description_. Defaults to None.
+        X_train (DataFrame, optional): _description_. Defaults to None.
+        X_test (DataFrame, optional): _description_. Defaults to None.
+        train (bool, optional): _description_. Defaults to True.
+
+    Returns:
+        np.array: X_train_scaled, X_test_scaled if train==True
+                       data_scaled if train==False
+    """    
+    
+    if train ==True:
+        scaler = StandardScaler().fit(X_train)
+        joblib.dump(scaler, './src/features/scaler.pkl')
+
+        X_train_scaled = scaler.transform(X_train)
+        X_test_scaled = scaler.transform(X_test)
+
+        return X_train_scaled, X_test_scaled
+    
+    else:
+        scaler = joblib.load('./src/features/scaler.pkl')
+        data_scaled = scaler.transform(data)
+
+        return data_scaled
+
+
+def preprocess(data: DataFrame,
+               train: bool=True,
+               save: bool=True,
+               path: str = './data/processed/',
+               label_col: str = 'label',
+               optimal_features: list=[
+                'timestamp', 'ip_len', 'ip_id', 'ip_flags', 'ip_ttl', 'ip_proto',
+                'ip_checksum', 'ip_dst', 'ip_dst_host','tcp_srcport', 'tcp_dstport',
+                'tcp_port', 'tcp_stream', 'tcp_completeness', 'tcp_seq_raw', 'tcp_ack',
+                'tcp_ack_raw', 'tcp_flags_reset', 'tcp_flags_syn', 'tcp_window_size_value',
+                'tcp_window_size', 'tcp_window_size_scalefactor', 'tcp_', 'udp_srcport',
+                'udp_dstport', 'udp_port', 'udp_length', 'udp_time_delta', 'eth_dst_oui',
+                'eth_addr_oui', 'eth_dst_lg', 'eth_lg', 'eth_ig', 'eth_src_oui', 'eth_type',
+                'icmp_type', 'icmp_code', 'icmp_checksum', 'icmp_checksum_status', 'arp_opcode'
+                ],
+                label_mapping: dict={
+                    'normal':0, 'dos_synflooding':1, 'mirai_ackflooding':2, 'host_discovery':3,
+                    'telnet_bruteforce':4, 'mirai_httpflooding':5, 'mirai_udpflooding':6,
+                    'mitm_arpspoofing':7, 'scanning_host':8, 'scanning_port':9, 'scanning_os':10
+                    }):
+
+    if train==True:
+        data = get_features(data, optimal_features, train=train)
+        data = label_data(data, label_col, label_mapping)
+        data = undersample_data(data, label_col)
+        data = convert_to_float(data)
+        X_train, X_test, y_train, y_test = split(data, label_col)
+        X_train_scaled, X_test_scaled = scale_data(X_train=X_train,
+                                                   X_test=X_test,
+                                                   train=train)
+        
+        if save==True:
+            np.savetxt(str(path+'X_train_scaled.csv'), X_train_scaled, delimiter=',')
+            np.savetxt(str(path+'X_test_scaled.csv'), X_test_scaled, delimiter=',')
+            y_train.to_csv(str(path+'y_train.csv'), index=False, header=True, mode='w')
+            y_test.to_csv(str(path+'y_test.csv'), index=False, header=True, mode='w')
+
+
+        return X_train_scaled, X_test_scaled, y_train, y_test
+    
+    else:
+        data = get_features(data, optimal_features, train=False)
+        data = convert_to_float(data)
+        data = scale_data(data=data, train=False)
+
+        return data
+    
+
+if __name__=='__main__':
+
+    raw_data = pd.read_csv('./data/labelled/all_data_labelled.csv')
+
+    X_train_scaled, X_test_scaled, y_train, y_test = preprocess(data=raw_data,
+                                                                train=True,
+                                                                save=True)
+    
+
+    print('train features shape:')
+    print(X_train_scaled.shape)
+
+    print('\ntest features shape:')
+    print(X_test_scaled.shape)
+
+    print("\ntrain targets shape")
+    print(y_train.shape)
+
+    print("\ntest targets shape")
+    print(y_test.shape)   
+
